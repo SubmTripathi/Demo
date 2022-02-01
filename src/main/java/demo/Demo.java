312651b2a1e7770b736f2c6fe96513fcfd021cc7
saran <saran@192.168.137.1>
2022-02-01 12:59:04 +0530

    My first Commit

---
+++ src/main/java/demo/Demo.java
@@ -0,0 +1,14 @@
+package demo;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Demo {
+	public static void main(String[] args)
+	{
+		System.setProperty("webdriver.chrome.driver", "C:\\Users\\saran\\OneDrive\\Desktop\\chromedriver.exe");
+		WebDriver driver = new ChromeDriver();
+		driver.get("https://www.google.com/");
+	}
+
+}
